d28104fc8cf5fd87a2ff2d38b311cef2d5f2f1c8
Maris Usis <marisusisme@gmail.com>
2016-08-18 21:49:50 -0400

    update

--- src/net/spacegeek224/metrobukkit/MetroPlugin.java
+++ src/net/spacegeek224/metrobukkit/MetroPlugin.java
@@ -8,16 +8,18 @@ public class MetroPlugin extends JavaPlugin {
 	// Fired when plugin is first enabled
     @Override
     public void onEnable() {
+    	this.saveDefaultConfig();
     	registerCommands();
     	getServer().getLogger().info("MetroPlugin is up and running!");
     }
     // Fired when plugin is disabled
     @Override
     public void onDisable() {
+    	this.saveConfig();
     	getServer().getLogger().info("MetroPlugin is shutting down...");
     }
     
     public void registerCommands() {
-    	this.getCommand("goto").setExecutor(new CommandGoTo());
+    	this.getCommand("goto").setExecutor(new CommandGoTo(this));
     }
 }
