2030f45651036f46f078969e5eaf5468366db701
Maris Usis <marisusisme@gmail.com>
2016-08-21 15:06:28 -0400

    update

--- src/net/spacegeek224/metrobukkit/command/CommandGoTo.java
+++ src/net/spacegeek224/metrobukkit/command/CommandGoTo.java
@@ -3,6 +3,7 @@ package net.spacegeek224.metrobukkit.command;
 import java.util.List;
 import java.util.Map;
 
+import org.bukkit.ChatColor;
 import org.bukkit.Location;
 import org.bukkit.World;
 import org.bukkit.command.Command;
@@ -16,11 +17,11 @@ import org.bukkit.event.player.PlayerTeleportEvent.TeleportCause;
 import net.spacegeek224.metrobukkit.MetroPlugin;
 
 public class CommandGoTo implements CommandExecutor {
-	
+
 	public FileConfiguration config;
 	public World w;
 	private MetroPlugin p;
-	
+
 	public CommandGoTo(MetroPlugin p) {
 		this.p = p;
 		config = p.getConfig();
@@ -30,27 +31,31 @@ public class CommandGoTo implements CommandExecutor {
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
 		if (sender instanceof Player) {
 			Player player = (Player) sender;
-			Map<String, Object> locations = config.getConfigurationSection("goto").getValues(true);
-			ConfigurationSection s = (ConfigurationSection)locations.get(args[0]);
-			
-			String toWorld = s.getString("world");
-			String toX = s.getString("x");
-			String toY = s.getString("y");
-			String toZ = s.getString("z");
-			
-			Location toLocation = new Location(p.getServer().getWorld(toWorld), s.getInt(toX), s.getInt(toY), s.getInt(toZ));
-			player.sendMessage("Sending you to: "+ toWorld+" - "+"("+toX+","+toY+","+toZ+")...");
-			player.teleport(toLocation);
-			// player.teleport(toLocation,TeleportCause.UNKNOWN);
-			
-			// Here we need to give items to our player
+			if (args.length == 1) {
+				Map<String, Object> locations = config.getConfigurationSection("goto").getValues(true);
+				ConfigurationSection s = (ConfigurationSection) locations.get(args[0]);
+
+				String toWorld = s.getString("world");
+				String toX = s.getString("x");
+				String toY = s.getString("y");
+				String toZ = s.getString("z");
+
+				Location toLocation = new Location(p.getServer().getWorld(toWorld), s.getInt(toX), s.getInt(toY),
+						s.getInt(toZ));
+				player.sendMessage("Sending you to: " + toWorld + " - " + "(" + toX + "," + toY + "," + toZ + ")...");
+				player.teleport(toLocation);
+				// player.teleport(toLocation,TeleportCause.UNKNOWN);
+			} else {
+				return false;
+			}
+
 		} else {
-			
+			sender.sendMessage(
+					new StringBuilder(ChatColor.GOLD.toString()).append("Only players can do this!").toString());
 		}
 		// If the player (or console) uses our command correct, we can return
 		// true
 		return true;
 	}
-
 
 }
