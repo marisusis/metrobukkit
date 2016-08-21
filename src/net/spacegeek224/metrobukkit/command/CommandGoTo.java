d9e353043c561650a84bff1936049ea70be31d85
Maris Usis <marisusisme@gmail.com>
2016-08-21 13:51:07 -0400

    Config

--- src/net/spacegeek224/metrobukkit/command/CommandGoTo.java
+++ src/net/spacegeek224/metrobukkit/command/CommandGoTo.java
@@ -4,7 +4,6 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.configuration.file.FileConfiguration;
-import org.bukkit.configuration.file.FileConfigurationOptions;
 import org.bukkit.entity.Player;
 
 import net.spacegeek224.metrobukkit.MetroPlugin;
@@ -20,10 +19,12 @@ public class CommandGoTo implements CommandExecutor {
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
 		if (sender instanceof Player) {
 			Player player = (Player) sender;
-			player.sendMessage(config.getStringList("goto").get(0));
+			player.sendMessage("Sending you to..."+config.getString("goto.location1.x"));
+			
 			// Here we need to give items to our player
+		} else {
+			sender.sendMessage("Sending you to..."+config.getString("goto.location1.x"));
 		}
-
 		// If the player (or console) uses our command correct, we can return
 		// true
 		return true;
