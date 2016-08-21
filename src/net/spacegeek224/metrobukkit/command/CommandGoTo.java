8048d592e695e1d55d220bbf07b52d23e9883710
Maris Usis <marisusisme@gmail.com>
2016-08-21 16:01:28 -0400

    update

--- src/net/spacegeek224/metrobukkit/command/CommandGoTo.java
+++ src/net/spacegeek224/metrobukkit/command/CommandGoTo.java
@@ -1,5 +1,8 @@
 package net.spacegeek224.metrobukkit.command;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.List;
 import java.util.Map;
 
@@ -10,6 +13,7 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.configuration.InvalidConfigurationException;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.player.PlayerTeleportEvent.TeleportCause;
@@ -21,36 +25,55 @@ public class CommandGoTo implements CommandExecutor {
 	public FileConfiguration config;
 	public World w;
 	private MetroPlugin p;
+	private Map<String, Object> locations;
 
 	public CommandGoTo(MetroPlugin p) {
+
 		this.p = p;
-		config = p.getConfig();
-		w = p.getServer().getWorld("world");
+		FileConfiguration config;
+		try {
+			config.load(new File(p.getDataFolder(),"goto.yml"));
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InvalidConfigurationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		locations = config.getConfigurationSection("locations").getValues(true);
 	}
 
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
 		if (sender instanceof Player) {
 			Player player = (Player) sender;
 			if (args.length == 1) {
-				Map<String, Object> locations = config.getConfigurationSection("goto").getValues(true);
-				ConfigurationSection s = (ConfigurationSection) locations.get(args[0]);
+				if (locations.containsKey(args[0])) {
+					ConfigurationSection s = (ConfigurationSection) locations.get(args[0]);
 
-				String toName = s.getName();
-				String toWorld = s.getString("world");
-				int toX = s.getInt("x");
-				int toY = s.getInt("y");
-				int toZ = s.getInt("z");
+					String toName = s.getName();
+					String toWorld = s.getString("world");
+					int toX = s.getInt("x");
+					int toY = s.getInt("y");
+					int toZ = s.getInt("z");
 
-				Location toLocation = new Location(p.getServer().getWorld(toWorld), toX, toY, toZ);
-				// player.sendMessage(new
-				// StringBuilder(ChatColor.GREEN.toString()).append("Sending you
-				// to: " + toWorld + " - " + "(" + toX + "," + toY + "," + toZ +
-				// ")...").toString());
-				player.sendMessage(new StringBuilder(ChatColor.GREEN.toString()).append("Sending you to ")
-						.append(ChatColor.YELLOW.toString()).append(toName).append(ChatColor.GREEN.toString())
-						.append("...").toString());
-				player.teleport(toLocation);
-				// player.teleport(toLocation,TeleportCause.UNKNOWN);
+					Location toLocation = new Location(p.getServer().getWorld(toWorld), toX, toY, toZ);
+					// player.sendMessage(new
+					// StringBuilder(ChatColor.GREEN.toString()).append("Sending
+					// you
+					// to: " + toWorld + " - " + "(" + toX + "," + toY + "," +
+					// toZ +
+					// ")...").toString());
+					player.sendMessage(new StringBuilder(ChatColor.GREEN.toString()).append("Sending you to ")
+							.append(ChatColor.YELLOW.toString()).append(toName).append(ChatColor.GREEN.toString())
+							.append("...").toString());
+					player.teleport(toLocation);
+					// player.teleport(toLocation,TeleportCause.UNKNOWN);
+				} else {
+					player.sendMessage(new StringBuilder(ChatColor.RED.toString()).append("I can't seem to find ").append(ChatColor.YELLOW.toString()).append(args[0]).append(ChatColor.RED.toString()).append(".").toString());
+				}
 			} else {
 				return false;
 			}
